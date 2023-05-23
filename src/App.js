7d3315c054ccc702d2f8759155ad10e34f7fe562
mitesh] <miteshap94@gamil.com>
2023-05-23 22:53:00 +0530

    features todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+      const [name , setName] = useState("")
+      const [allData , setAllData] = useState([])
+
+
+      const handleadd = () =>{
+
+        // console.log(name);
+
+        if(name.trim() !== ""){
+
+          setAllData(perv => [...perv,name])
+        }
+        setName("")
+      }
+      // console.log(allData);
+
+      // const handledelete = (index) =>{
+      //     allData.splice(index,1)
+      //     // const remove =  user.filter(item => item !== user);
+      //     // setAllData([...remove]);
+      //     setAllData([...allData])
+      // }
+    
+      const handleDelete = (index) => {
+        const updatedData = allData.filter((item,id) => id !== index);
+        setAllData(updatedData);
+      }
+      
+      
+      
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Todo List</h1>
+    <input type="name"  onChange={(e) =>setName(e.target.value)} value={name}/>
+    <button onClick={handleadd}>ADD</button>
+    {allData.length === 0 ? (
+        <div>
+          <h3>Empty Data is here</h3>
+        </div>
+      ) : (
+        allData.map((val, i) => (
+          <div key={i}>
+            <h3>{val}</h3>
+            <button>Edit</button>
+            <button onClick={() => handleDelete(i)}>Delete</button>
+          </div>
+        ))
+      )}
+
     </div>
   );
 }
